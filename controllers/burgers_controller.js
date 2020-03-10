a889d4cbe8b9267b49d9daf8049d633d88bcd270
phil cowan <philipjcowan1@gmail.com>
2020-03-09 22:46:32 -0700

    writes burgers_controllers.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,42 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger")
+
+router.get("/:", function(req, res){
+    burger.all(function(data){
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/cats", function(req, res) {
+    burger.insert([
+        "burger_name", "devoured"
+    ],[
+        req.body.name, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertID });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " +req.params.id;
+    console.log("condition", condition);
+    
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result){
+        if (result.changedRows == 0) {
+            return res.status(404).end()
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
